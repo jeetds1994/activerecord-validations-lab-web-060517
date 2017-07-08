91f51d33f615dc62eb79eef07d9c4a7a8c23609e
Diljeet <jeetds1994@gmail.com>
2017-07-08 11:27:18 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :summary, length: {maximum: 250}
+  validates :content, length: {minimum: 250}
+  validates :category, inclusion: %w(Fiction Non-fiction)
+  before_validation :check_title
+
+  def check_title
+    return false if self.title.nil?
+    answer = false
+    arr = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+    arr.each {|title| answer = true if self.title.include?(title)}
+    answer
+  end
+
 end
